bed26c08c8658d29b5803da26da231e4bc85785f
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-03-23 16:20:07 +0900

    connect with r2

--- src/commands/clip.js
+++ src/commands/clip.js
@@ -1,5 +1,6 @@
 import { ContentType } from "@prisma/client";
 import { db } from "../../lib/db.js";
+import { uploadToR2 } from "../../lib/r2.js";
 
 export async function clip(interaction) {
     // ファイルを取得
@@ -8,11 +9,19 @@ export async function clip(interaction) {
     if (interaction.commandName === "clip") {
         const attachment = interaction.options.getAttachment("file");
         try {
+            const res = await fetch(attachment.url);
+            const buffer = Buffer.from(await res.arrayBuffer());
+
+            const url = await uploadToR2(
+                buffer,
+                attachment.name,
+                attachment.contentType
+            )
             await db.content.create({
                 data : {
                         channelId : interaction.channelId,
                         type : ContentType.CDN,
-                        url : attachment.url,
+                        url : url,
                         createdAt : new Date(),
                         updatedAt : new Date(),
                 }
